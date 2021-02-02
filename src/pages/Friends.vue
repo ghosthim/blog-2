0e2c80d1ddc1222d1d35feadfbb8429578796953
LeeYoungJe <344667237@qq.com>
2021-02-02 20:22:11 +0800

    fix: personal change

--- src/pages/Friends.vue
+++ src/pages/Friends.vue
@@ -13,9 +13,6 @@
         ><g-image class="avatar" alt="avatar" :src="friend.avatar" blur="5" />
         <span>{{ friend.id }}</span></a
       >
-      <p class="note">
-        * I don't accept friend link requests from someone I don't know.
-      </p>
     </div>
 
     <div class="post-comments">
@@ -25,7 +22,7 @@
 </template>
 
 <script>
-import DisqusJS from 'disqusjs'
+
 
 export default {
   metaInfo: {
@@ -35,74 +32,12 @@ export default {
     return {
       friends: [
         {
-          id: '@Felinae',
-          link: 'https://code.felinae98.cn/',
-          avatar: 'https://avatars3.githubusercontent.com/u/23295345?s=160',
-          style: 'color: #fff; background-color: #473922',
-        },
-        {
-          id: '@agnoCJY',
-          link: 'https://jychuuu.com/',
-          avatar: 'https://avatars1.githubusercontent.com/u/46088026?s=160',
-          style: 'color: #fff; background-color: #191919',
-        },
-        {
-          id: '@TenkeySeven',
-          link: 'https://blog.tenkeyseven.com/',
-          avatar: 'https://avatars2.githubusercontent.com/u/33371927?s=160',
-          style: 'color: #fff; background-color: #b59672',
-        },
-        {
-          id: '@Silvester',
-          link: 'https://silvester.wang/',
-          avatar: 'https://avatars0.githubusercontent.com/u/34436920?s=160',
-          style: 'color: #fff; background-color: #595058',
-        },
-        {
-          id: '@ash0ne',
-          link: 'https://blog.ash0ne.com/',
-          avatar: 'https://avatars3.githubusercontent.com/u/28522665?s=160',
-          style: 'color: #fff; background-color: #9f8cd1',
-        },
-        {
-          id: '@FKY',
-          link: 'http://blog.fkynjyq.com/',
-          avatar: 'https://avatars2.githubusercontent.com/u/16451516?s=160',
-          style: 'color: #fff; background-color: #005240',
-        },
-        {
-          id: '@idealclover',
-          link: 'https://idealclover.top/',
-          avatar: 'https://avatars2.githubusercontent.com/u/24428416?s=160',
-          style: 'color: #fff; background-color: #487747',
-        },
-        {
-          id: '@kastnerorz',
-          link: 'https://github.com/kastnerorz',
-          avatar: 'https://avatars3.githubusercontent.com/u/26199342?s=160',
-          style: 'color: #fff; background-color: #26498e',
-        },
-        {
           id: '@Patrick Wu',
           link: 'https://patrickwu.space/',
           avatar: 'https://avatars0.githubusercontent.com/u/15316889?s=160',
           style: 'color: #fff; background-color: #61a3cf',
         },
       ],
-    }
-  },
-  mounted() {
-    // Initialize post comment by DisqusJS
-    if (process.env.NODE_ENV === 'production') {
-      const disqusjs = new DisqusJS({
-        shortname: 'spencerwoo',
-        siteName: "Spencer's Blog",
-        identifier: 'spencer-blog-guestbook',
-        apikey:
-          'F6hHeFWtfmWW5n4RVf4hjgazRj8y0ERfQdeQPIGKr79yajw6glnmTqrgYHTC8XaS',
-        admin: 'spencerwoo',
-        adminLabel: 'Admin',
-      })
     }
   },
 }
